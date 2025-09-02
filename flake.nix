52c40d5023aa3cb67ab4fd646c98e6990adbfb79
Patima Poochai <patimapoochai@gmail.com>
2025-09-02 13:28:35 -1000

    Change nixos host configuration to use flakes

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Configurations for home lab machines";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./NixOS/nixos/configuration.nix
+      ];
+    };
+
+  };
+}
