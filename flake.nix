ade32d952f290f8b1c5e37f4a7f647ed49630cdb
Patima Poochai <patimapoochai@gmail.com>
2025-09-02 14:38:57 -1000

    Change nixpkgs URL in root flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Configurations for home lab machines";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
