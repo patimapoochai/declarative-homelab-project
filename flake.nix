3af289514a562055c0181e080c3a805822777505
Patima Poochai <patimapoochai@gmail.com>
2025-10-16 22:26:44 -1000

    Added sops files for testing

--- flake.nix
+++ flake.nix
@@ -1,16 +1,19 @@
 {
   description = "Configurations for home lab machines";
+  # this is here just to point NixOS to the correct configs in the ./NixOS folder
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-
+  outputs = { self, nixpkgs, sops-nix ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./NixOS/nixos/configuration.nix
+        sops-nix.nixModules.sops
       ];
     };
 
