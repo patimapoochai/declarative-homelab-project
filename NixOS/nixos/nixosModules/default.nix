dc250a68fb2c34fae931ece3314270eaa0ccd005
Patima Poochai <patimapoochai@gmail.com>
2025-10-16 22:52:39 -1000

    Add sops configuration to the nix files

--- NixOS/nixos/nixosModules/default.nix
+++ NixOS/nixos/nixosModules/default.nix
@@ -6,5 +6,6 @@
     ./k3s.nix
     ./prometheus-node-exporter.nix
     ./admin-utils.nix
+    ./sops.nix
   ];
 }
---
+++ NixOS/nixos/nixosModules/sops.nix
@@ -0,0 +1,10 @@
+{
+  sops.defaultSopsFile = ./secrets/nixos.yml;
+  sops.defaultSopsFormat = "yaml";
+
+  # This will automatically import SSH keys as age keys
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+
+  # This is the actual specification of the secrets.
+  sops.secrets."nixos/openssh_authorizedkey" = {};
+}
