e2e492b05fc1cfd6b58dabd89473103003709a79
Patima Poochai <patimapoochai@gmail.com>
2025-12-03 21:09:23 -1000

    Sops key change and minor fixes

--- NixOS/nixos/nixosModules/ansible-managed-setup.nix
+++ NixOS/nixos/nixosModules/ansible-managed-setup.nix
@@ -10,7 +10,9 @@
   users.users.ansible = {
     isNormalUser = true;
     home = "/home/ansible";
-    openssh.authorizedKeys.keys = [${config.sops.templates."nixos-openssh-authorizedkey.toml".path}]; # Add with ansible lineinfile module
+    openssh.authorizedKeys.keys = [
+      "${config.sops.templates."nixos-openssh-authorizedkey.toml".path}"
+      ]; # Add with ansible lineinfile module
   };
 
   # set up sudo
--- NixOS/nixos/nixosModules/k3s.nix
+++ NixOS/nixos/nixosModules/k3s.nix
@@ -4,7 +4,7 @@
   services.k3s = {
     enable = true;
     role = "agent";
-    token = ${config.sops.templates."nixos-k3s-token.toml".path}; # Add with ansible lineinfile
-    serverAddr = ${config.sops.templates."nixos-k3s-serverAddr.toml".path}; # Add with ansible lineinfile
+    token = config.sops.templates."nixos-k3s-token.toml".path; # Add with ansible lineinfile
+    serverAddr = config.sops.templates."nixos-k3s-serverAddr.toml".path; # Add with ansible lineinfile
   };
 }
