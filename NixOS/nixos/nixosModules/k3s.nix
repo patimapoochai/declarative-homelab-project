9b5d66e782475c54cc86fa23f9c0f3e6c0a40773
Patima Poochai <patimapoochai@gmail.com>
2025-12-10 12:57:53 -1000

    test insecure sops-nix inline secrets option in k3s

--- NixOS/nixos/nixosModules/k3s.nix
+++ NixOS/nixos/nixosModules/k3s.nix
@@ -6,5 +6,6 @@
     role = "agent";
     tokenFile = config.sops.templates."nixos-k3s-token.toml".path; # Add with ansible lineinfile
     environmentFile = config.sops.templates."nixos-k3s-env.toml".path; # Add with ansible lineinfile
+    # serverAddr = "${builtins.readFile config.sops.secrets."nixos/k3s/serverAddr".path}";
   };
 }
